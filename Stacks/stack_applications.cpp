20e71d5f1665f1eb1599b8e40d8dd7e30bca917d
acoustic-reaper <110325044+acoustic-reaper@users.noreply.github.com>
2022-12-26 15:40:31 +0530

    Added files via upload

---
+++ Stacks/stack_applications.cpp
@@ -0,0 +1,16 @@
+/*
+ * Applications of Stacks:
+ *   1) Reversal of a string 
+ *   2) Reversal of a linked list 
+*/
+
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
